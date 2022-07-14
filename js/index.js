c42391f73e9aa07b25bdd54b782e0adf942e3b43
k-n-lee <100110086+k-n-lee@users.noreply.github.com>
2022-07-13 23:21:23 -0400

    Revisions 1.

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("section").scroll(function(){
+        $("heroImg").fadeIn();
+        $("MySkills").fadeIn("slow");
+        $("SelectWork").fadeIn(3000);
+    });
+  });
